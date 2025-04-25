16c75ad5039e55fa7a041b8201d79633368e1a5f
ChoSeoongHo <shcho@pntbiz.com>
2025-04-25 09:49:37 +0900

    jenkins job DSL

---
+++ jobs/seedJob.groovy
@@ -0,0 +1,24 @@
+job('seed-job') {
+    description('모든 Job DSL을 실행하여 Jenkins Job을 동기화')
+
+    scm {
+        git {
+            remote {
+                url('git@github.com:ChoSeoongHo/pntbiz-jenkins.git')
+                credentials('ssh-pntbiz-jenkins')
+            }
+            branch('main')
+        }
+    }
+
+    steps {
+        dsl {
+            external('jobs/test.groovy')
+            removeAction('DELETE')
+        }
+    }
+
+    triggers {
+        scm('H/5 * * * *')
+    }
+}
