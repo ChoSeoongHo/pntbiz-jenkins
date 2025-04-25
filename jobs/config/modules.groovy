f554ad72167132e21595098dac682e2f367e3674
ChoSeoongHo <shcho@pntbiz.com>
2025-04-25 13:32:44 +0900

    Fix gradle job

--- jobs/config/modules.groovy
+++ jobs/config/modules.groovy
@@ -7,6 +7,6 @@ return [
         oauth             : 'pntbiz-oauth',
         so_pm             : 'pntbiz-so-pm',
         so_ts             : 'pntbiz-so-ts',
-        raas_efm          : 'pntbiz-raas-efm',
+        efm               : 'pntbiz-raas-efm',
         scanner_management: 'pntbiz-scanner-manager'
 ]
