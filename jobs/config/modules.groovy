2633d288530cd58eeda587b38b6453fb36816b36
ChoSeoongHo <shcho@pntbiz.com>
2025-04-25 11:23:29 +0900

    Add modules

--- jobs/config/modules.groovy
+++ jobs/config/modules.groovy
@@ -1,7 +1,12 @@
 return [
-        api   : 'pntbiz-api',
-        wms   : 'pntbiz-wms',
-        admin : 'pntbiz-admin',
-        oauth : 'pntbiz-oauth',
-        socket: 'pntbiz-socket',
+        api               : 'pntbiz-api',
+        wms               : 'pntbiz-wms',
+        admin             : 'pntbiz-admin',
+        rtls              : 'pntibz-rtls',
+        socket            : 'pntbiz-socket',
+        oauth             : 'pntbiz-oauth',
+        so_pm             : 'pntbiz-so-pm',
+        so_ts             : 'pntbiz-so-ts',
+        raas_efm          : 'pntbiz-raas-efm',
+        scanner_management: 'pntbiz-scanner-manager'
 ]
