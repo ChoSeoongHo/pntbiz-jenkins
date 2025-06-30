986023f1089c3d5160b37a16b1b9befee311e3fc
ChoSeoongHo <shcho@pntbiz.com>
2025-06-30 10:35:26 +0900

    Add server matrix

--- jobs/config/serverMatrix.groovy
+++ jobs/config/serverMatrix.groovy
@@ -4,6 +4,12 @@ package config
  * 서버별로 생성할 Job 정보
  */
 return [
-        'dev-10': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
-        'dev-23': ['efm']
+        'dev-02': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
+        'dev-05': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
+        'dev-14': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
+        'dev-15': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
+        'dev-17': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
+        'dev-18': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
+        'dev-20': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
+        'dev-24': ['api', 'wms', 'admin', 'rtls', 'socket', 'oauth'],
 ]
