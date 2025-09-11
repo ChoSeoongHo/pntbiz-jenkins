166338f0b99584298577803ede7139dd3f02841e
smhong <smhong@pntbiz.com>
2025-09-11 10:01:06 +0900

    prod-sejong rtls 추가

--- jobs/config/serverMatrix.groovy
+++ jobs/config/serverMatrix.groovy
@@ -47,5 +47,5 @@ return [
         'smartcare'        : ['api', 'wms', 'admin', 'socket', 'oauth', 'rtls'],
         'ssyenc'           : ['api', 'wms', 'admin', 'socket', 'rtls'],
         'yh'               : ['api_v3', 'socket_v3', 'admin_v3', 'wms_v3', 'rtls', 'ct'],
-        'prod-sejong'      : ['api', 'wms', 'admin', 'socket'],
+        'prod-sejong'      : ['api', 'wms', 'admin', 'socket', 'rtls'],
 ]
