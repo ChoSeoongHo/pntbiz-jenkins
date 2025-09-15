44eee2daa1d084f5c38b20e9087e40e4ba9ce43e
ChoSeoongHo <shcho@pntbiz.com>
2025-09-15 13:18:02 +0900

    socket v3 수정

--- jobs/templates/nodejs/socket_v3.groovy
+++ jobs/templates/nodejs/socket_v3.groovy
@@ -5,7 +5,8 @@ return { Map config ->
         description(config.description)
 
         parameters {
-            stringParam('BRANCH', config.defaultBranch ?: 'pjt-02-develop', '브랜치 : [브랜치명]\n태그 :  refs/tags/[태그번호]')
+            stringParam('REPOSITORY', 'pntbiz-server.git', '')
+            stringParam('BRANCH', config.defaultBranch ?: 'pjt-01-develop', '브랜치 : [브랜치명]\n태그 :  refs/tags/[태그번호]')
             stringParam('VERSION', config.version ?: 'v12', '')
             stringParam('CONFIG', config.config ?: 'dev/common', 'config 하위 폴더 넣기')
             stringParam('SHELL', config.shell ?: 'dev', 'sh 하위 폴더 넣기')
@@ -36,34 +37,34 @@ return { Map config ->
                         DEPLOY_DIR_NAME=socket
                         
                         echo "----------- Make temp directory -----------"
-                        rm -rf ${DEPLOY_DIR_NAME}
-                        mkdir -p ${DEPLOY_DIR_NAME}
-                        mkdir -p ${DEPLOY_DIR_NAME}/config/${CONFIG}
+                        rm -rf \${DEPLOY_DIR_NAME}
+                        mkdir -p \${DEPLOY_DIR_NAME}
+                        mkdir -p \${DEPLOY_DIR_NAME}/config/\${CONFIG}
                         
                         echo "----------- cp file -----------"
-                        cp README.md ${DEPLOY_DIR_NAME}
+                        cp README.md \${DEPLOY_DIR_NAME}
                         
                         echo "----------- cp file -----------"
-                        if [[ "$REPOSITORY" == "pntbiz-server.git" ]];then
-                        cp -R pntbiz-root/pntbiz-socket/config/${CONFIG}/* ${DEPLOY_DIR_NAME}/config/${CONFIG}
-                        cp pntbiz-root/pntbiz-socket/config/config.js ${DEPLOY_DIR_NAME}/config/
-                        cp pntbiz-root/pntbiz-socket/sh/${SHELL}/*.sh ${DEPLOY_DIR_NAME}
-                        cp pntbiz-root/pntbiz-socket/*.js ${DEPLOY_DIR_NAME}
-                        cp pntbiz-root/pntbiz-socket/*.json ${DEPLOY_DIR_NAME}
+                        if [[ "\$REPOSITORY" == "pntbiz-server.git" ]];then
+                        cp -R pntbiz-root/pntbiz-socket/config/\${CONFIG}/* \${DEPLOY_DIR_NAME}/config/\${CONFIG}
+                        cp pntbiz-root/pntbiz-socket/config/config.js \${DEPLOY_DIR_NAME}/config/
+                        cp pntbiz-root/pntbiz-socket/sh/\${SHELL}/*.sh \${DEPLOY_DIR_NAME}
+                        cp pntbiz-root/pntbiz-socket/*.js \${DEPLOY_DIR_NAME}
+                        cp pntbiz-root/pntbiz-socket/*.json \${DEPLOY_DIR_NAME}
                         else
-                        cp -R pntbiz-socket/config/${CONFIG}/* ${DEPLOY_DIR_NAME}/config/${CONFIG}
-                        cp pntbiz-socket/config/config.js ${DEPLOY_DIR_NAME}/config/
-                        cp pntbiz-socket/sh/${SHELL}/*.sh ${DEPLOY_DIR_NAME}
-                        cp pntbiz-socket/*.js ${DEPLOY_DIR_NAME}
-                        cp pntbiz-socket/*.json ${DEPLOY_DIR_NAME}
+                        cp -R pntbiz-socket/config/\${CONFIG}/* \${DEPLOY_DIR_NAME}/config/\${CONFIG}
+                        cp pntbiz-socket/config/config.js \${DEPLOY_DIR_NAME}/config/
+                        cp pntbiz-socket/sh/\${SHELL}/*.sh \${DEPLOY_DIR_NAME}
+                        cp pntbiz-socket/*.js \${DEPLOY_DIR_NAME}
+                        cp pntbiz-socket/*.json \${DEPLOY_DIR_NAME}
                         fi
                         
                         echo "----------- Make zip file -----------"
-                        cd ${DEPLOY_DIR_NAME}
+                        cd \${DEPLOY_DIR_NAME}
                         /usr/local/bin/npm cache clean --force
                         /usr/local/bin/npm install
                         
-                        zip -r ${DEPLOY_DIR_NAME} *
+                        zip -r \${DEPLOY_DIR_NAME} *
                         nvm use default
                     """.stripIndent())
                 }
